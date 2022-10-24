a1d852505285852de9f46ce813ed371c237b5069
Temirbolat <snake2009003@mail.ru>
2022-10-24 22:08:25 +0600

    Initial commit

---
+++ settings/conf.py
@@ -0,0 +1,4 @@
+# -------------------------------
+# Environmental variables
+#
+SECRET_KEY = 'django-insecure-48jyzia6a!4ed42zq8ey(+5kxm2rm)a)*x0m)r=1nm$5rxt81%'
---
+++ settings/env/local.py
@@ -0,0 +1,25 @@
+from settings.base import *  # noqa
+
+
+# ---------------------------------
+#
+DEBUG = True
+WSGI_APPLICATION = None
+ASGI_APPLICATION = None
+
+# ---------------------------------
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
